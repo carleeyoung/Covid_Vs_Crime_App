e755f369996517182a813a2b27c848f562edb9f4
carleeyoung <61207381+carleeyoung@users.noreply.github.com>
2020-12-02 14:42:29 -0600

    Update files

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,75 @@
-// Uncomment below
 function buildPlot() {
-  /* data route */
-  const url = "/api/pals";
-  d3.json(url).then(function(response) {
+    // Read in json 
+    d3.json('/data').then(function(data) {
+        console.log(data)
+        // Get covid cases and crime cases from file
+        var scatterData = [];
+        for (var i = 0; i <data[0].length; i++) {
+            for (var j = 0; j < data[1].length; j++) {
+                for (var k = 0; k < data[2].length; k++) {
+                    if (data[0][i].zipcode === data[1][j].zipcode & data[0][i].zipcode === data[2][k].zipcode) {
+                        scatterData.push({"zipcode": "_" + data[0][i].zipcode + "_", 
+                        "covid_cases": data[0][i].covid_cases,
+                        "crime_cases": data[1][j].crime_cases,
+                        "population": data[2][k].population});
+                    }
+                }    
+            }
+        };
+        console.log(scatterData);
+        zipcode = scatterData.map(({ zipcode }) => zipcode)
+        console.log(zipcode);
+        covid_cases = scatterData.map(({ covid_cases }) => covid_cases)
+        console.log(covid_cases);
+        crime_cases = scatterData.map(({ crime_cases }) => crime_cases)
+        console.log(crime_cases);
+        population = scatterData.map(({ population }) => population)
+        console.log(population);
 
-    console.log(response);
+        var trace1 = {
+            x: covid_cases,
+            y: crime_cases,
+            mode: "markers",
+            type: "scatter",
+            text: zipcode,
+            name: "Crime"
+          };
 
-    const data = response;
+        var trace2 = {
+            x: covid_cases,
+            y: population,
+            yaxis: 'y2',
+            mode: "markers",
+            type: "scatter",
+            color: "red",
+            text: zipcode,
+            name: "Pop."
+          };
+    
+        var data1 = [trace1, trace2];
+        var layout1 = {
+                title: "Covid Cases vs. Crime Cases & Population by Zipcode",
+                xaxis: {title: "Covid Cases"},
+                yaxis: {title: "Crime Cases"},
+                yaxis2: {
+                    title: "Population",
+                    overlaying: 'y',
+                    side: 'right'
+                },
+                legend: {
+                    orientation:"h",
+                    yanchor:"bottom",
+                    y:1.05,
+                    xanchor:"center",
+                    x:0.5
+                    }
+            }
+        Plotly.newPlot("img1", data1, layout1)
+    
+    });
 
-    const layout = {
-      scope: "usa",
-      title: "Pet Pals",
-      showlegend: false,
-      height: 600,
-            // width: 980,
-      geo: {
-        scope: "usa",
-        projection: {
-          type: "albers usa"
-        },
-        showland: true,
-        landcolor: "rgb(217, 217, 217)",
-        subunitwidth: 1,
-        countrywidth: 1,
-        subunitcolor: "rgb(255,255,255)",
-        countrycolor: "rgb(255,255,255)"
-      }
-    };
-
-    Plotly.newPlot("plot", data, layout);
-  });
+    
+      
 }
 
 buildPlot();
